b005f5dfc5b4952de22a2f870a249397ca23ce0a
Sebastian Bernauer <sbernauer@united-internet.de>
2018-11-15 14:26:18 +0100

    Removed code to benchmark CrossProductNode

--- src/main/java/nodes/expression/arithmetic/CrossProductNode.java
+++ src/main/java/nodes/expression/arithmetic/CrossProductNode.java
@@ -17,22 +17,19 @@ public class CrossProductNode extends ExpressionNode {
         this.right = right;
     }
 
-    private static Vector3 crossProduct(Vector3 left, Vector3 right) {
-        INDArray a = left.getNdArray();
-        INDArray b = right.getNdArray();
+    @Override
+    public Variable execute(SymbolTable symbolTable) {
+        Variable leftValue = left.execute(symbolTable);
+        Variable rightValue = right.execute(symbolTable);
 
-        int size = a.shape()[0];
-        double[] result = new double[size * 3];
-
-        for (int i = 0; i < size; i++) {
-            result[i * 3 + 0] = a.getDouble(i, 1) * b.getDouble(i, 2) - a.getDouble(i, 2) * b.getDouble(i, 1);
-            result[i * 3 + 1] = a.getDouble(i, 2) * b.getDouble(i, 0) - a.getDouble(i, 0) * b.getDouble(i, 2);
-            result[i * 3 + 2] = a.getDouble(i, 0) * b.getDouble(i, 1) - a.getDouble(i, 1) * b.getDouble(i, 0);
+        if (leftValue instanceof Vector3 && rightValue instanceof Vector3) {
+            return crossProduct((Vector3) leftValue, (Vector3) rightValue);
+        } else {
+            throw new RuntimeException("Can't do cross product because of wrong types");
         }
-        return new Vector3(Nd4j.create(result, new int[]{size, 3}));
     }
 
-    private static Vector3 crossProductSubArray(Vector3 left, Vector3 right) {
+    private Vector3 crossProduct(Vector3 left, Vector3 right) {
         INDArray a = left.getNdArray();
         INDArray b = right.getNdArray();
 
@@ -57,43 +54,19 @@ public class CrossProductNode extends ExpressionNode {
         return new Vector3(result);
     }
 
-    public static void main(String[] args) {
+    private Vector3 crossProductSlow(Vector3 left, Vector3 right) {
+        INDArray a = left.getNdArray();
+        INDArray b = right.getNdArray();
 
-        //Erster Durchlauf: Warm up; Letzter Durchlauf: Messung
-        for (int i = 0; i < 5; i++) {
+        int size = a.shape()[0];
+        double[] result = new double[size * 3];
 
-            //Benchmark
-            Vector3 left =  new Vector3(Nd4j.rand(10000, 3));
-            Vector3 right = new Vector3(Nd4j.rand(10000, 3));
-
-            long startTme = System.currentTimeMillis();
-            Vector3 result1 = crossProductSubArray(left, right);
-            System.out.println("Time crossProduct: " + (System.currentTimeMillis() - startTme));
-            startTme = System.currentTimeMillis();
-            Vector3 result2 = crossProduct(left, right);
-            System.out.println("Time crossProductSubArray: " + (System.currentTimeMillis() - startTme));
-
-            for (int x = 0; x < left.getNdArray().shape()[0]; x++) {
-                for (int y = 0; y < 3; y++) {
-                    double difference = result1.getNdArray().getDouble(x, y) - result2.getNdArray().getDouble(x, y);
-                    if (difference > 10e-7) {
-                        throw new RuntimeException("Value at " + x + ", " + y + " differs.");
-                    }
-                }
-            }
+        for (int i = 0; i < size; i++) {
+            result[i * 3 + 0] = a.getDouble(i, 1) * b.getDouble(i, 2) - a.getDouble(i, 2) * b.getDouble(i, 1);
+            result[i * 3 + 1] = a.getDouble(i, 2) * b.getDouble(i, 0) - a.getDouble(i, 0) * b.getDouble(i, 2);
+            result[i * 3 + 2] = a.getDouble(i, 0) * b.getDouble(i, 1) - a.getDouble(i, 1) * b.getDouble(i, 0);
         }
-    }
-
-    @Override
-    public Variable execute(SymbolTable symbolTable) {
-        Variable leftValue = left.execute(symbolTable);
-        Variable rightValue = right.execute(symbolTable);
-
-        if (leftValue instanceof Vector3 && rightValue instanceof Vector3) {
-            return crossProductSubArray((Vector3) leftValue, (Vector3) rightValue);
-        } else {
-            throw new RuntimeException("Cant cross product because of wrong types");
-        }
+        return new Vector3(Nd4j.create(result, new int[]{size, 3}));
     }
 }
 
